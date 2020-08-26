83f2a69bb498888fb7a19c1d76f2b38ea94e9027
dufferbrady <92andrew.brady@gmail.com>
2020-08-26 15:42:12 +1000

    styled the top bar of the user dashboard page

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -2,10 +2,27 @@ import React from "react";
 
 import classes from "./Dashboard.module.css";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+
 function Dashboard() {
   return (
     <div className={classes.container}>
-      <div className={classes.top_bar}>Dashboard</div>
+      <div className={classes.top_bar}>
+        <div className={classes.top_left_container}>
+          <span className={classes.top_left1}>Dashboard</span>
+          <div className={classes.top_left2}>
+            <span className={classes.dashboard_span1}>Workouts</span>
+            <span className={classes.dashboard_span2}>Weight</span>
+          </div>
+        </div>
+        <div className={classes.top_right}>
+          <button className={classes.dashboard_button}>
+            <FontAwesomeIcon className={classes.dashboard_icon} icon={faPlus} />
+            Add Workout
+          </button>
+        </div>
+      </div>
       <div className={classes.bottom_bar}>
         <div className={classes.bottom_left}>profile pic</div>
         <div className={classes.bottom_center}>recent workouts</div>
